5b0d1e7c3727290dfd543f94489ae8ecb1463615
Shreyas Ashtamkar <shreyu@programmer.net>
2020-12-16 16:57:55 +0530

    " + VERSION \n + ADD \n + SHOW \n + SHOW_ONLY \n + SHOW_ALL \n + [DETAILED]"

--- filemark/command_line.py
+++ filemark/command_line.py
@@ -1,27 +1,40 @@
-from .filemarkutils import args, bookmarkEntry, quitTerminal as _quitTerminal, _isValid, _tabulate
+from .filemarkutils import args, bookmarkEntry, quitTerminal as _quitTerminal, _isValid, _tabulate, saveBookmark, getBookmarks
 
 
 # CASE 1 - Display Version
 if args.version:
-    print("Version 0.1")
-    exit(0)
+	print("Version 0.1")
+	exit(0)
 
 
 # CASE 2 - Show Only Specific File/Folder
 if args.show_only:
-	print("Still Running")
+	output = getBookmarks(args.ITEM)
+	if output == []:
+		print(f"None of these found --> {args.ITEM}")
+		exit(1)
+	else:
+		table = _tabulate(
+			output, 
+			headers=["S.No.","Name", "Path"], 
+			tablefmt="pretty", 
+		)
+		print(table)
+		exit(0)
+
+
+# CASE 3 - Show all the bookmarks
+if args.show_all:
+	table = _tabulate(
+		getBookmarks(), 
+		headers=["S.No.","Name", "Path"], 
+		tablefmt="pretty", 
+		showindex=True
+	)
+	print(table)
 	exit(0)
 
-
-if args.show:
-    with open(".bookmarks") as bm_file:
-        table = _tabulate([row.split('|') for row in bm_file.readlines()], headers=["S.No.","Name", "Path"], tablefmt="pretty", showindex=True)
-        print(table)
-    exit(0)
-
-print("\n\n\n", args.add)
-
+# CASE 4 - Add a new bookmark to the list, automatically
 if args.add:
-    with open(".bookmarks", 'a') as bm_file:
-        bm_file.write(bookmarkEntry(args.ITEM[0]))
-    exit(0)
+	fileName = args.ITEM[0]
+	saveBookmark(fileName)
--- filemark/filemarkutils.py
+++ filemark/filemarkutils.py
@@ -1,4 +1,4 @@
-from .              import filemarkutils    as _utils
+from typing import List
 from os             import kill             as _os_kill
 from os             import getcwd           as _pwd
 from os             import getppid          as _os_getppid
@@ -20,14 +20,19 @@ _parser.add_argument(
 	"-a"                ,
 	"--add"             ,
 	action="store_true" ,
-	default=True		,
 	help="Bookmark the specific folder"
 )
 _parser.add_argument(
 	"-v"                , 
 	"--version"         ,
-	action="store_true" , 
+	action="store_true" ,
 	help="Display the version information of filemark."
+)
+_parser.add_argument(
+	"-s"                ,
+	"--show"            ,
+	action="store_true" ,
+	help="Smart 'show' option, shows the results if asked to search else throws entire list on screen."
 )
 _parser.add_argument(
 	"--show-only"       ,
@@ -35,12 +40,84 @@ _parser.add_argument(
 	help="Show the details of some specific bookarked item."
 )
 _parser.add_argument(
-	"-s"                ,
-	"--show"            ,
 	"--show-all"        ,
 	action="store_true" ,
-	help="Show the list of currently bookmarked items."
+	help="Show the entire list of currently bookmarked items."
 )
+_parser.add_argument(
+	"--detailed"        ,
+	action="store_true" ,
+	help="Show extra detailed output "
+)
+
+
+
+
+# Collect Arguments and count ITEMS
+args	= _parser.parse_args()
+argc	= len(args.ITEM)
+
+if not any((args.add, args.show, args.version, args.show_only, args.show_all)):
+	args.add = True
+elif args.show:
+	if any((args.version, args.show_only, args.show_all, args.add)):
+		raise Exception("incompatible options")
+	elif argc > 0:
+		args.show = False
+		args.show_only = True
+	elif argc == 0:
+		args.show = False
+		args.show_all = True
+
+# CHECK ERRORS
+_WMESG = []
+
+# for debugging
+# print(args)
+
+try:
+	if args.add:
+		if any((args.version, args.show_only, args.show, args.detailed, args.show_all)):
+			raise Exception("incompatible options")
+		elif argc < 1:
+			raise Exception("--add requires a file/folder as an argumet to bookmark")
+		elif argc > 1:
+			_WMESG.append("Too many Items in --add. using ONLY the first argument.")
+
+	elif args.version:
+		if any((args.add, args.show_only, args.show, args.show_all)):
+			raise Exception("incompatible options")
+		elif argc > 0:
+			raise Exception("--version takes no arguments")
+	
+
+	elif args.show_only:
+		if any((args.version, args.add, args.show, args.show_all)):
+			raise Exception("incompatible options.")
+		elif argc < 1:
+			raise Exception("--show-only requires atleast \n\t\t one argument to show the details of. \n\n Did you mean --show-all ?")
+	
+	elif args.show_all:
+		if any((args.version, args.add, args.show, args.show_only)):
+			raise Exception("incompatible options.")
+		elif argc > 1:
+			raise Exception("--show-all takes no arguments.\n\n Did you mean --show-only ?")
+	
+	else:
+		print("UNREACHABLE FROM CODE. FATAL")
+		exit(1)
+
+
+except Exception as e:
+	_parser.error("<FATAL> " + str(e) + "\n\nFor additional help type filemark -h") 
+
+
+
+#display all the warning messages
+for msg in _WMESG:
+	print("<WARNING> :",msg)
+
+
 
 
 def _getPath(fileName:str = None) -> any:
@@ -50,43 +127,15 @@ def _getPath(fileName:str = None) -> any:
 		return None
 
 
-
-
-
-# Collect Arguments and count ITEMS
-args	= _parser.parse_args()
-argc	= len(args.ITEM)
-
-
-# CHECK ERRORS
-_ERROR = False
-_EMESG = "Other Error"
-
-
-if any((args.add, args.show_only)) and argc < 1:
-	_ERROR = True
-	_EMESG = "\n<FATAL> File / Folder not supplied"
-
-if not args.add and argc > 1:
-	_EMESG = "\n<WARNING> Only using the first supplied Folder/File to bookmark."
-
-
-if _ERROR:
-	_parser.error(_EMESG + "\n\n for help type filemark -h")
-
-
-
-
-
-
-
-
-
 # Accessible from external Files
+
+
 
 def quitTerminal():
     input("This is Cool !! Press Enter to Exit ...")
     _os_kill(_os_getppid(), _SIGHUP)
+
+
 
 def bookmarkEntry(fileName:str = None) -> any:
 	if fileName is None:
@@ -94,5 +143,73 @@ def bookmarkEntry(fileName:str = None) -> any:
 	
 	filePath = _getPath(fileName)
 
-	return filePath + "|" + fileName
-	
+	return "\n" + fileName + "|" + filePath
+
+
+
+def getBookmarks(selector:str=None) -> any:
+	'''
+	Show the bookmarks stored over time by the user.
+	'''
+	with open(".bookmarks") as bm_file:
+		table = []
+		for row in bm_file.readlines():
+			row = row.replace('"', '')
+			row = row.strip().split('|')
+			if len(row) == 2:
+				bm_name, bm_path = row
+				bm_path = bm_path.split('/')
+				if len(bm_path)>4 and not args.detailed:
+					bm_path = '.../' + '/'.join(bm_path[-2:])
+				else:
+					bm_path = '/'.join(bm_path)
+				table.append((bm_name, bm_path))
+
+		if selector in (None, ""):
+			return table
+		
+		elif isinstance(selector, list):
+			small_table = []
+			for search_item in selector:
+				if search_item.isdigit():
+					no = int(search_item)
+					if no < len(table):
+						no, bm_name, bm_path = no,table[no][0],table[no][1]
+						small_table.append([no, bm_name, bm_path])
+				else:
+					for i, row in enumerate(table):
+						bm_name, bm_path = row
+						if search_item in bm_name:
+							small_table.append([i+1, bm_name, bm_path])
+			return small_table
+		
+		elif isinstance(selector, str):
+			for index, bm_path, bm_name in enumerate(table):
+				if bm_name == selector:
+					selector = index
+					break
+			else:
+				return table
+					
+			return [table[selector]]
+		else:
+			return table
+		
+
+
+def saveBookmark(fileName:str = None) -> bool:
+	'''
+	Store a new bookmark
+
+	param:
+		`fileName`: Name of the file/folder to bookmark
+	'''
+
+	if fileName is None:
+		raise TypeError("fileName cannot be None")
+
+	if not _isValid(fileName):
+		raise FileNotFoundError(fileName+" Not found.")
+		
+	with open(".bookmarks", 'a') as bm_file:
+		bm_file.write(bookmarkEntry(fileName))
